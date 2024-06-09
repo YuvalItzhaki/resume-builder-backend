32c7ba0690e9abf15a65bb19b0ca3e20fe9484fb
yuvalitzhaki <yyuval1971@gmail.com>
2024-06-09 17:13:42 +0300

    set the redirect after register user to the dashboard

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -25,7 +25,7 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     // Successful authentication, redirect home.
-    res.redirect('/dashboard'); // Adjust the redirect URL as needed
+    res.redirect('http://localhost:5173/dashboard'); // Adjust the redirect URL as needed
   }
 );
 
