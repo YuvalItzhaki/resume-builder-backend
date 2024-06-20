fb31f8d023941c338b075c7b5d77eb3c02896d36
yuvalitzhaki <yyuval1971@gmail.com>
2024-06-20 16:01:36 +0300

    fixed the profileRoutes issue - the profile wasnt saved in the db

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -3,33 +3,31 @@ const router = express.Router();
 const Resume = require('../models/resumeModel');
 
 router.get('/resumes', async (req, res) => {
-    try {
-        const resume = await Resume.findOne();
-        res.json(resume.profile);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching profile data', error });
-    }
+  try {
+    const resume = await Resume.findOne();
+    res.json({ profile: resume.profile });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching profile data', error });
+  }
 });
 
 router.put('/resumes/profile', async (req, res) => {
-    const updatedProfile = req.body;
+  const { profile } = req.body;
 
-    try {
-        let resume = await Resume.findOne(); // Adjust the query as necessary
+  try {
+    let resume = await Resume.findOne();
 
-        if (!resume) {
-            return res.status(404).json({ message: 'Resume not found' });
-        }
-
-        // Update the education information within the resume
-        resume.profile = updatedProfile;
-
-        await resume.save();
-
-        res.json({ message: 'Profile updated successfully', profile: resume.profile });
-    } catch (error) {
-        res.status(500).json({ message: 'Error updating profile data', error });
+    if (!resume) {
+      return res.status(404).json({ message: 'Resume not found' });
     }
+
+    resume.profile = profile;
+    await resume.save();
+
+    res.json({ message: 'Profile updated successfully', profile: resume.profile });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating profile data', error });
+  }
 });
 
 module.exports = router;
